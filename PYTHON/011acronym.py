9237dd414975154ce0fa256cc9f8d3ed8dce05cc
ALLANKAKAI <allankakai26@gmail.com>
2019-06-25 06:24:38 +0300

    acronym test

---
+++ PYTHON/011acronym.py
@@ -0,0 +1,19 @@
+
+def convert():
+    x = input("Enter you Phrase:")
+    y = x.upper()
+    print("y:", y)
+
+    for item in y[x]:
+        print(item)
+
+    y.split()
+    print("Split:", y)
+    letters = []
+
+
+
+
+
+
+convert()
